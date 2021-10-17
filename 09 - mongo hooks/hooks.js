518e351fffbfaa5dab273d6c9517fc3da95ad219
Pranav N <pranavbharadwaj142@gmail.com>
2021-10-17 14:01:18 +0530

    added mongo hooks and validations

---
+++ 09 - mongo hooks/hooks.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const mongoose= require("mongoose");
+
+const app = express();
+const emailValidator=require("email-validator"); // email validator
+// middleware function
+app.use(express.json());
+app.listen(3000);
+let users = [];
+
+const userRouter = express.Router();
+app.use("/user", userRouter);
+
+// mini app whose base route is /user
+userRouter
+  .route("/") //   .../user/
+  .get(getUsers)
+  .post(postUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+userRouter.route("/:id").get(getUserById);
+
+
+const authRouter = express.Router();
+app.use("/auth", authRouter); // global middleware
+//auth
+authRouter
+  .route("/signup")
+  .get(middleware, getSignUp) // path specific middleware
+  .post(postSignUp);
+
+function middleware(req, res, next) {
+  console.log("middleware encountered"); // middleware
+  next();
+}
+
+async function getUsers(req, res) {
+
+  let allUsers=await userModel.find();
+  //let user=await userModel.findOne({name:"pranav"});
+
+  res.json({message:'list of all users',data:allUsers});
+}
+
+function postUser(req, res) {
+ // console.log(req.body);
+  users = req.body;
+  res.json({
+    message: "data received successfully",
+    user: req.body,
+  });
+}
+async function updateUser(req, res) {
+  console.log("req.body ->",req.body);
+  let dataToBeUpdated = req.body;
+  let user=await userModel.findOneAndUpdate('abc@gmail.com',dataToBeUpdated)
+ 
+  res.json({
+    message: "data updated successfully",
+  });
+}
+
+async function deleteUser(req, res) {
+  let dataToBeDeleted=req.body
+  console.log("delete -> ",dataToBeDeleted);
+  let user=await userModel.findOneAndDelete(dataToBeDeleted);
+  res.json({
+    message: "data has been deleted",
+    data:user
+  });
+}
+
+function getUserById(req, res) {
+  res.send("user id is", req.params);
+  console.log(req.params.id);
+}
+
+function getSignUp(req, res) {
+
+  res.sendFile("/public/index.html", { root: __dirname });
+}
+ async function postSignUp(req, res) {
+  let dataObj=req.body;
+  let user=await userModel.create(dataObj);
+  console.log("backend", user);
+  res.json({
+    message: "user signedup",
+    data: user,
+  });
+}
+const db_link='mongodb+srv://<NAME>:<PASSWORD>@cluster0.m8o2x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(db_link)
+.then(function(db){
+  console.log('db connected');
+  //console.log(db);
+})
+.catch(function (err){
+  console.log(err);
+})
+
+const userSchema=mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+    validate:function(){
+      return emailValidator.validate(this.email) // we have used external package to validate email
+    }
+  },
+  password:{
+    type:String,
+    required:true,
+    minLength:8
+
+  },  
+  confirmPassword:{
+    type:String,
+    required:true,
+    minLength:8,
+    validate:function(){
+      return this.confirmPassword==this.password  //validator were we confirm password
+    }
+
+  }
+});
+// mongo hooks were we will delete confirm password as it is of no use
+userSchema.pre('save',function(){
+  console.log("before save in db",this);
+  this.confirmPassword=undefined;
+})
+// hooks called after saving our db
+userSchema.post('save',function(doc){
+  console.log("after save in db",doc);
+})
+
+
+//model
+const userModel=mongoose.model('userModel',userSchema);
+
+
+
